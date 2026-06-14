51dfb12772eb3aa535ac841203cf19fe69799ec8
Vanessa Latchem-Smith <vanessa.latchem@outlook.com>
2026-06-14 18:01:31 +0100

    useAI: allow recommend_meal_plan through action validation

--- src/hooks/useAI.ts
+++ src/hooks/useAI.ts
@@ -60,6 +60,7 @@ function validateAction(action: unknown): action is Action {
     case 'set_goals':
     case 'recommend_recipe':
     case 'body_scan_prompt':
+    case 'recommend_meal_plan':
       return true // server-validated; trust
     case 'recommend_workout':
       return validateRecommendWorkout(a.payload)
